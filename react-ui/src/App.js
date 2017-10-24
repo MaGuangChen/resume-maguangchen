35010639aab30d2037e5f14c1ca9853a3b05f290
Paul Ma <kwn791122@gmail.com>
2017-10-25 01:27:32 +0800

    edit intro container style

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import * as actions from './actions/actions';
 
 import Intro from './containers/Intro';
 import About from './containers/About';
 import './styles/App.css';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
-  // componentDidMount() {
-  // }
   render() {
     return (
       <div className="App">
