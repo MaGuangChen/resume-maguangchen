60c61b1508a901c2211161f6157e87a069af316d
Paul Ma <kwn791122@gmail.com>
2017-10-25 05:50:41 +0800

    finished portifolio container

--- react-ui/src/components/portifolio/Gallery.js
+++ react-ui/src/components/portifolio/Gallery.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const Gallery = (props) => {
-    const { background, text } = props;
+    const { background, title, text, link } = props;
     const backgroundStyle = {
         background: `url(${background})center no-repeat`,
         backgroundSize: 'cover'
     }
+    const linkTo = (e) => {
+        e.preventDefault();
+        window.open(`${link}`,'_blank');
+    }
     return (
-        <div style={backgroundStyle}>
+        <div onClick={linkTo} style={backgroundStyle}>
             <p>
+                <a href={link} target="_blank">{title}</a>
                 <a>{text}</a>
             </p>
         </div>
--- react-ui/src/containers/Portifolio.js
+++ react-ui/src/containers/Portifolio.js
@@ -1,35 +1,52 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import Gallery from '../components/portifolio/Gallery';
-import settourNew from '../images/settour-new.png';
+import settourOld from '../images/settour-old.jpg';
+import settourBlog from '../images/settour-blog.png';
+import reactWeather from '../images/react-weather.png';
+import mySketch from '../images/my-sketch.jpg';
 
 const Portifolio = (props) => {
+    const linkTo = (e) => {
+        e.preventDefault();
+        window.open('https://ticket.settour.com.tw/search','_blank');
+    }
     return (
         <div className="portifolio">
             <p className="portifolio_title">Portifolio</p>
             <div className="portifolio_block">
-                <div className="portifolio_block_left">
+                <div onClick={linkTo} className="portifolio_block_left">
                         <p>
                             <a>產品平台前端開發</a>
+                            <a>東南旅遊新版網站的票券產品線平台開發，
+                            使用React、Redux、GraphQL、Webpack、Express、Mocha
+                            </a>
                         </p>
                 </div>
                 <div className="portifolio_block_right">
                     <Gallery 
-                      background={settourNew}
-                      text="產品平台開發"
+                      background={settourOld}
+                      title="網站與APP"
+                      text="東南旅遊既有網站與APP前端維護"
+                      link='https://www.settour.com.tw/'
                     />
                     <Gallery 
-                      background={settourNew}
-                      text="產品平台開發"
+                      background={settourBlog}
+                      title="Blog"
+                      text="東南旅遊的行銷活動頁面與部落格協助"
+                      link='http://blog.settour.com.tw/'
                     />
                     <Gallery 
-                      background={settourNew}
-                      text="產品平台開發"
+                      background={reactWeather}
+                      title="Side Project"
+                      text="React氣象查詢APP"
+                      link='https://paul-react-weather-app.herokuapp.com/'
                     />
                     <Gallery 
-                      background={settourNew}
-                      text="產品平台開發"
+                      background={mySketch}
+                      title="Sketch"
+                      text="興趣使然"
+                      link='https://www.instagram.com/p/BGjwT60tK1v/?hl=zh-tw&taken-by=ma_guang_chen'
                     />
                 </div>
             </div>
@@ -37,4 +54,4 @@ const Portifolio = (props) => {
     )
 }
 
-export default connect()(Portifolio);
+export default Portifolio;
